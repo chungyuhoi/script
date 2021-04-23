53c11e6dd51de12318a3ef28779d417372c01572
chungyuhoi <79551245+chungyuhoi@users.noreply.github.com>
2021-04-23 14:44:09 +0800

    Add files via upload

--- utqemu.sh
+++ utqemu.sh
@@ -664,7 +664,14 @@ read -r -p "1)tcg 2)自动检测 3)tcg缓存指定(测试用勿选) " input
 	case $input in
 		1) 
 set -- "${@}" "-machine" "pc,usb=off,vmport=off,dump-guest-core=off,kernel-irqchip=off" "--accel" "tcg,thread=multi" ;;
-		3) set -- "${@}" "-machine" "pc,usb=off,vmport=off,dump-guest-core=off,kernel-irqchip=off" "--accel" "tcg,thread=multi,tb-size=$(($(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq | awk '{print $1/100000}' | cut -d '.' -f 1)*100))" ;;
+3) echo -e "${RED}注意！设置tcg的缓存可以提高模拟效率，以m为单位，跟手机闪存ram也有关系(调高了会出现后台杀)，请谨慎设置${RES}"
+	echo -n -e "请输入拟缓存的数值(以m为单位，例如1800)，回车为默认值，请输入: "
+	read TB
+       if [ -n "$TB" ]; then
+			set -- "${@}" "-machine" "pc,usb=off,vmport=off,dump-guest-core=off,kernel-irqchip=off" "--accel" "tcg,thread=multi,tb-size=$TB"
+		else
+			set -- "${@}" "-machine" "pc,usb=off,vmport=off,dump-guest-core=off,kernel-irqchip=off" "--accel" "tcg,thread=multi,tb-size=$(($(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq | awk '{print $1/100000}' | cut -d '.' -f 1)*100))"
+			fi	;;
 		*) set -- "${@}" "-machine" "pc,accel=kvm:xen:hax:tcg,usb=off,vmport=off,dump-guest-core=off,kernel-irqchip=off" ;;
 	esac ;;
 					*)
@@ -677,7 +684,14 @@ esac ;;
 read -r -p "1)tcg 2)自动检测 3)tcg缓存指定(测试用勿选) " input
 case $input in
 	1) set -- "${@}" "-machine" "q35,usb=off,vmport=off,dump-guest-core=off,kernel-irqchip=off" "--accel" "tcg,thread=multi" ;;
-	3) set -- "${@}" "-machine" "q35,usb=off,vmport=off,dump-guest-core=off,kernel-irqchip=off" "--accel" "tcg,thread=multi,tb-size=$(($(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq | awk '{print $1/100000}' | cut -d '.' -f 1)*100))" ;;
+	3) echo -e "${RED}注意！设置tcg的缓存可以提高模拟效率，以m为单位，跟手机闪存ram也有关系(调高了会出现后台杀)，请谨慎设置${RES}"
+		echo -n -e "请输入拟缓存的数值(以m为单位，例如1800)，回车为默认值，请输入: "
+		read TB
+		if [ -n "$TB" ]; then
+			set -- "${@}" "-machine" "q35,usb=off,vmport=off,dump-guest-core=off,kernel-irqchip=off" "--accel" "tcg,thread=multi,tb-size=$TB"
+		else
+		set -- "${@}" "-machine" "q35,usb=off,vmport=off,dump-guest-core=off,kernel-irqchip=off" "--accel" "tcg,thread=multi,tb-size=$(($(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq | awk '{print $1/100000}' | cut -d '.' -f 1)*100))"
+		fi	;;
 	*) set -- "${@}" "-machine" "q35,accel=kvm:xen:hax:tcg,usb=off,vmport=off,dump-guest-core=off,kernel-irqchip=off" ;;
 esac ;;
 					*)
@@ -790,9 +804,11 @@ fi
 #	set -- "${@}" "-append" "cmdline"
 	case $QEMU_SYS in
 		qemu-system-i386)
-#取消高精度定时器,仅i386
+#取消高精度定时器
 	set -- "${@}" "-no-hpet"
+#取消软盘启动检测
 	set -- "${@}" "-no-fd-bootchk"
+#取消高级配置与电源管理
 #        set -- "${@}" "-no-acpi"
 ;;
 		*) ;;
@@ -817,7 +833,7 @@ esac
 echo -e "请选择${YELLOW}cpu${RES}"
 case $SYS in
 	QEMU_ADV|ANDROID)
-		read -r -p "1)n270 2)athlon 3)pentium2 4)core2duo 5)Skylake-Server-IBRS 6)Nehalem-IBRS 7)Opteron_G5 8)Dhyana 9)测试用(勿选) " input ;;
+		read -r -p "1)n270 2)athlon 3)pentium2 4)core2duo 5)Skylake-Server-IBRS 6)Nehalem-IBRS 7)Opteron_G5 8)Dhyana 9)测试用(勿选) 0)推荐" input ;;
 QEMU_PRE) read -r -p "1)n270 2)athlon 3)pentium2 4)core2duo 5)Skylake-Server-IBRS 6)Nehalem-IBRS 7)Opteron_G5 " input ;;
 esac
 #部分cpu id flags：fpu –板载FPU，vme –虚拟模式扩展，de –调试扩展，pse –页面大小扩展，tsc –时间戳计数器，操作系统通常可以得到更为精准的时间度量，msr –特定于模型的寄存器，pae –物理地址扩展，cx8 – CMPXCHG8指令，apic–板载APIC，sep– SYSENTER/SYSEXIT，mtrr –存储器类型范围寄存器，pge – Page Global Enable，mca –Machine Check Architecture，cmov – CMOV instructions（附加FCMOVcc，带有FPU的FCOMI），pat –页面属性表，pse36 – 36位PSE，clflush – CLFLUSH指令，dts –调试存储，acpi –ACPI via MSR，mmx –多媒体扩展，fxsr – FXSAVE/FXRSTOR, CR4.OSFXSR，sse – SSE，sse2 – SSE2，ss – CPU自侦听，ht –超线程，tm –自动时钟控制，ia64 – IA-64处理器，pbe –等待中断启用，mmxext – AMD MMX扩展，fxsr_opt – FXSAVE / FXRSTOR优化，rdtscp – RDTSCP，lm –长模式（x86-64），3dnowext – AMD 3DNow扩展，k8 –皓龙，速龙64，k7 –速龙，pebs –基于精确事件的采样，bts –分支跟踪存储，nonstop_tsc – TSC不会在C状态下停止，PNI – SSE-3，pclmulqdq – PCLMULQDQ指令，dtes64 – 64位调试存储，监控器–监控/等待支持，ds_cpl – CPL Qual.调试存储，vmx –英特尔虚拟化技术(VT技术)，smx –更安全的模式，est –增强的SpeedStep，tm2 –温度监控器2，ssse3 –补充SSE-3，cid –上下文ID，cx16 – CMPXCHG16B，xptr –发送任务优先级消息，dca –直接缓存访问，sse4_1 – SSE-4.1，sse4_2 – SSE-4.2，x2apic – x2APIC，aes – AES指令集，xsave – XSAVE / XRSTOR / XSETBV / XGETBV，avx –高级矢量扩展，hypervisor–在hypervisor上运行，svm –AMD的虚拟化技术(AMD-V)，extapic –扩展的APIC空间，cr8legacy – 32位模式下的CR8，abm –高级bit操作，ibs –基于Sampling的采样，sse5 – SSE-5，wdt –看门狗定时器，硬件锁定清除功能（HLE），受限事务存储（RTM）功能，HLE与RTM为TSX指令集，决定服务器cpu多线程或单线程处理数据。
@@ -872,8 +888,8 @@ esac
 			set -- "${@}" "-smp" "8,cores=8,threads=1,sockets=1"
 			fi ;;
 		*) set -- "${@}" "-cpu" "max"
-			if [ -n "$CPU" ]; then
-				set -- "${@}" "-smp" "$CPU"
+			if [ -n "$_SMP" ]; then
+				set -- "${@}" "-smp" "$_SMP"
 			else
 				set -- "${@}" "-smp" "4"
 				fi ;;
@@ -892,8 +908,8 @@ esac
 		set -- "${@}" "-smp" "4,cores=4,threads=1,sockets=1"
 		fi ;;
         *)      set -- "${@}" "-cpu" "max"
-		if [ -n "$CPU" ]; then
-                set -- "${@}" "-smp" "$CPU"
+		if [ -n "$_SMP" ]; then
+                set -- "${@}" "-smp" "$_SMP"
 	else
 		set -- "${@}" "-smp" "4"
 		fi ;;
@@ -948,7 +964,7 @@ case $input in
 	0) set -- "${@}" "-net" "none" ;;
 esac
 
-			echo -e "请选择${YELLOW}声卡${RES}(不加载则提升模拟效率)"
+			echo -e "请选择${YELLOW}声卡${RES}(不加载可提升模拟效率)"
 			read -r -p "1)ac97 2)sb16 3)es1370 4)hda 0)不加载 " input
                         case $input in
                 1|"") set -- "${@}" "-soundhw" "ac97" ;;
@@ -994,7 +1010,6 @@ read -r -p "1)cirrus 2)vmware 3)std 4)virtio 5)qxl " input
 			1|"")
 #				set -- "${@}" "-vga" "virtio"
 				set -- "${@}" "-device" "virtio-vga"
-#                       set -- "${@}" "-device" "virtio-vga"
 #                       set -- "${@}" "-device" "virtio-vga,virgl=on"
 ;;
 			2) echo -e "\n${YELLOW}你选择virtio显卡3D参数，该模式只能在图形界面(桌面)显示${RES}"
@@ -1005,8 +1020,7 @@ read -r -p "1)cirrus 2)vmware 3)std 4)virtio 5)qxl " input
 						set -- "${@}" "-vga" "qxl" "-display" "gtk,gl=on" "-device" "virtio-gpu-pci,virgl=on"
 #                               set -- "${@}" "-device" "qxl" "-vga" "virtio" "-display" "gtk,gl=on"
 ;;
-					spice) set -- "${@}" "-device" "virtio-vga" "-display" "gtk,gl=on" ;;
-					amd|gtk_) set -- "${@}" "-device" "virtio-vga" "-display" "gtk,gl=on" ;;
+					spice|amd|gtk_) set -- "${@}" "-device" "virtio-vga" "-display" "gtk,gl=on" ;;
 				esac
 				unset display
 				case $ARCH in
@@ -1049,7 +1063,7 @@ case $input in
 		set -- "${@}" "-netdev" "user,id=user0" ;;
 	0) set -- "${@}" "-net" "none" ;;
 esac
-echo -e "请选择${YELLOW}声卡${RES}(不加载则提升模拟效率)"
+echo -e "请选择${YELLOW}声卡${RES}(不加载可提升模拟效率)"
 		read -r -p "1)es1370 2)sb16 3)hda 4)ac97(推荐) 0)不加载 " input
                         case $input in
                         1) set -- "${@}" "-device" "ES1370" ;;
